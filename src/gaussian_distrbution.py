24bb9e568f1221e8019cddb7a0ff69d545e8d563
adhamelkhouly <adham.elkhouly@hotmail.com>
2019-07-11 15:48:26 -0400

    removed redundant function and fixed a calculate_stdev bug

--- src/gaussian_distrbution.py
+++ src/gaussian_distrbution.py
@@ -73,7 +73,7 @@ class Gaussian(Distribution):
         else:
             n = len(self.data)
 
-        mean = self.mean
+        mean = self.calculate_mean()
 
         sigma = 0
 
@@ -100,29 +100,3 @@ class Gaussian(Distribution):
 
         return (1.0 / (self.stdev * math.sqrt(2 * math.pi))) * math.exp(
             -0.5 * ((x - self.mean) / self.stdev) ** 2)
-
-    def read_data_file(self, file_name, sample=True):
-        """
-        Function to read in data from a txt file. The txt file should have
-        one number (float) per line. The numbers are stored in data attribute.
-        After reading in the file, the mean and standard dev are calculated
-
-        Args:
-            file_name (string): name of a file to read from
-            sample (bool): boolean for sample vs population
-
-        Returns:
-            None
-        """
-
-        with open(file_name) as file:
-            data_list = []
-            line = file.readline()
-            while line:
-                data_list.append(int(line))
-                line = file.readline()
-        file.close()
-
-        self.data = data_list
-        self.mean = self.calculate_mean()
-        self.stdev = self.calculate_stdev(sample)
